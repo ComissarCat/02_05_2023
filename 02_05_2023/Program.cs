0239068b7932d36dec43a322ebe33894315e4724
Медведев Андрей Юрьевич <andrey.medvede@yandex.ru>
2024-01-19 12:40:58 +0300

    Добавьте файлы проекта.

---
+++ 02_05_2023/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_05_2023
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Person> person = new List<Person>()
+            {
+                new Person(){ Name = "Andrey", Age = 24, City = "Kyiv" },
+                new Person(){ Name = "Liza", Age = 18, City = "Moscow" },
+                new Person(){ Name = "Oleg", Age = 15, City = "London" },
+                new Person(){ Name = "Sergey", Age = 55, City = "spb" },
+                new Person(){ Name = "Sergey", Age = 32, City = "spb" }
+            };
+            Console.WriteLine("Люди старше 25 лет:");
+            foreach (var item in (from p in person where p.Age > 25 select p).ToList())
+                Console.WriteLine(item);
+            Console.WriteLine();
+            Console.WriteLine("Люди, проживающие не в СПБ:");
+            foreach (var item in (from p in person where p.City != "spb" select p).ToList())
+                Console.WriteLine(item);
+            Console.WriteLine();
+            Console.WriteLine("Имена людей, проживающих в СПБ:");
+            foreach (var item in (from p in person where p.City == "spb" select p.Name).ToList())
+                Console.WriteLine(item);
+            Console.WriteLine();
+            Console.WriteLine("Люди старше 35 лет с именем Sergey:");
+            foreach (var item in (from p in person where p.Age > 25 && p.Name == "Sergey" select p).ToList())
+                Console.WriteLine(item);
+            Console.WriteLine();
+            Console.WriteLine("Люди, проживающие в Москве:");
+            foreach (var item in (from p in person where p.City == "Moscow" select p).ToList())
+                Console.WriteLine(item);
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public override string ToString()
+        {
+            return $"{Name}\t{Age}\t{City}";
+        }
+    }
+}
---
+++ 02_05_2023/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("02_05_2023")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("02_05_2023")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1c5c60ac-87a2-4a2b-b9c3-be81b454168e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
